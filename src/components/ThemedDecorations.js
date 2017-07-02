43b85569b2470668c6c317f652c69da8cdbdd2f6
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-07-02 11:34:29 +0000

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,16 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedDecorations extends React.Component {
+    render(){
+        const childrenWithAddedTheme = React.Children.map(this.props.children, child => {
+        return React.cloneElement(child, {
+          className: this.props.theme
+        });
+      });
+    return (
+      <div>
+        {childrenWithAddedTheme}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Invitation from './components/Invitation';
 import ThemedParty from './components/ThemedDecorations';
 
 ReactDOM.render(
+  //If we remove {this.props.children} from Invitation.js, the following text in between <Invitation> tag
+  //does not get rendred
   <div>
     <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
     <ThemedParty theme="heaven">
@@ -13,5 +15,5 @@ ReactDOM.render(
       <p>A last decoration</p>
     </ThemedParty>
   </div>,
-  document.getElementById('main')
+  document.getElementById('root')
 );
